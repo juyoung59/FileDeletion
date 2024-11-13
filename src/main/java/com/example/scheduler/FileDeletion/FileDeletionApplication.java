1cd7aee25ef9d72066336d49bdd793b83d313efe
juyoung59 <juyoung05@hanmail.net>
2024-11-13 10:44:29 +0900

    Initial

---
+++ src/main/java/com/example/scheduler/FileDeletion/FileDeletionApplication.java
@@ -0,0 +1,13 @@
+package com.example.scheduler.FileDeletion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileDeletionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileDeletionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/scheduler/FileDeletion/FileDeletionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scheduler.FileDeletion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileDeletionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
